bdccede8d5ffe8045ada3723f2fd1a25276e9cf4
yoavkamary <yoavkamary@gmail.com>
2017-03-09 12:27:31 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,52 @@
+var http = require("http");
+var fs = require("fs");
+var qs = require('querystring');
+
+
+http.createServer(function(req, res) {
+    console.log("recieving request");
+    console.log(`method = ${req.method}`);
+        console.log(`url = ${req.url}`);
+        console.log(`status = ${req.status}`);
+
+
+    var queryData = "";
+
+	if (req.method === "GET") {
+		res.writeHead(200, {"Content-Type": "text/html"});
+	    fs.createReadStream("./public/index.html", "UTF-8").pipe(res);
+	} else if (req.method === "POST") {
+
+		req.on("data", function(chunk) {
+			queryData += chunk;
+            
+            if (queryData.length > 1e6) { 
+                // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+                req.connection.destroy();
+            }
+        });
+
+		req.on("end", function() {
+            console.log("query data: " + queryData);
+            var post = qs.parse(queryData);
+            console.log(post.phonenumber);
+            if(post.phonenumber === "1234" ){
+                console.log("success!");
+                res.writeHead(200, {"Content-Type": "text/json"});
+                fs.createReadStream("./public/data.json", "UTF-8").pipe(res);
+            
+            } else {
+                res.writeHead(404, {"Content-Type": "text/plain"});
+                res.end("File Not Found");
+            }
+
+		});
+
+
+	}
+
+	
+
+}).listen(3000);
+
+console.log("Form server listening on port 3000");
