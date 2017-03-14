465c8090e947c7b9e7d6ca75d4c0a40b7f20e1b0
Yoav Kamary <Yoavkamary@gmail.com>
2017-03-14 16:49:56 +0200

    Create data on the fly, Server logs added

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 var http = require("http");
 var fs = require("fs");
 var qs = require('querystring');
+var exec = require('child_process').exec;
 
+exec('node ./Server/createData.js', function(error, stdout, stderr) {
+  if(error){
+    console.error(error.message);   
+  }
+    console.log(stdout);
+});
 
 http.createServer(function(req, res) {
     console.log("recieving request");
     console.log(`method = ${req.method}`);
-        console.log(`url = ${req.url}`);
-        console.log(`status = ${req.status}`);
-
+    console.log(`url = ${req.url}\n`);
 
     var queryData = "";
 
@@ -27,7 +32,7 @@ http.createServer(function(req, res) {
         });
 
 		req.on("end", function() {
-            console.log("query data: " + queryData);
+            console.log("Query data from POST request: " + queryData);
             var post = qs.parse(queryData);
             console.log(post.phonenumber);
             if(post.phonenumber === "1234" ){
@@ -36,6 +41,7 @@ http.createServer(function(req, res) {
                 fs.createReadStream("./Server/public/Data/data.json", "UTF-8").pipe(res);
             
             } else {
+                console.log("failure!");
                 res.writeHead(404, {"Content-Type": "text/plain"});
                 res.end("File Not Found");
             }
@@ -49,4 +55,5 @@ http.createServer(function(req, res) {
 
 }).listen(8080);
 
-console.log("server listening on port 8080");
+console.log("server listening on port 8080\n");
+console.log('Creating data');
