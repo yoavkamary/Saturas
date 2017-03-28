e92dcc3a511cd090f571a93ede4faa0cbba45aaf
Yoav Kamary <Yoavkamary@gmail.com>
2017-03-28 14:19:51 +0300

    Error fix

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ http.createServer(function(req, res) {
 
 	if (req.method === "GET") {
 		res.writeHead(200, {"Content-Type": "text/html"});
-	    fs.createReadStream(".Server/public/index.html", "UTF-8").pipe(res);
+	    fs.createReadStream("./Server/public/index.html", "UTF-8").pipe(res);
 	} else if (req.method === "POST") {
 
 		req.on("data", function(chunk) {
