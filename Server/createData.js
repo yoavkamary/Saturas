60522a70704d640a1db3f63794e0e75c1081a863
Yoav Kamary <Yoavkamary@gmail.com>
2017-03-28 12:36:22 +0300

    Adding properties to data sent from server, Setting dorpdowns and caption at index.html

--- Server/createData.js
+++ Server/createData.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var parse = require('csv-parse');
 var obj = {
+    info: { location: "", sensorId: "" }, 
     chartData0: [],
     chartData1: [],
     chartData2: [],
@@ -9,14 +10,20 @@ var obj = {
           };
 
 var parser = parse({from: 2}, function(err, data){
-  for(var i = 0; i < data.length; i++){  
-    var tempDate = dateObjectFromString(data[i][0]);
-    if(data[i][5] != '-') obj.chartData0.push({time: tempDate, precipitation: data[i][5]});
-    if(data[i][6] != '-') obj.chartData1.push({time: tempDate, potentialET: data[i][6]});
-    if(data[i][7] != '-') obj.chartData2.push({time: tempDate, temperature: data[i][7]});
-    if(data[i][8] != '-') obj.chartData3.push({time: tempDate, bar: data[i][8]});
-    if(data[i][9] != '-') obj.chartData4.push({time: tempDate, waterDeficit: data[i][9]});
-  }
+  
+    if(data.length >= 0){
+        obj.info.location = data[0][1];
+        obj.info.sensorId = data[0][4];
+    }
+    
+    for(var i = 0; i < data.length; i++){  
+        var tempDate = dateObjectFromString(data[i][0]);
+        if(data[i][5] != '-') obj.chartData0.push({time: tempDate, precipitation: data[i][5]});
+        if(data[i][6] != '-') obj.chartData1.push({time: tempDate, potentialET: data[i][6]});
+        if(data[i][7] != '-') obj.chartData2.push({time: tempDate, temperature: data[i][7]});
+        if(data[i][8] != '-') obj.chartData3.push({time: tempDate, bar: data[i][8]});
+        if(data[i][9] != '-') obj.chartData4.push({time: tempDate, waterDeficit: data[i][9]});
+      }
 
     fs.writeFile('./public/Data/data.json',JSON.stringify(obj),'UTF-8',function(err){
         if(err){
